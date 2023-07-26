46f517f7b072aeb383c4826a140ceb0d56faee65
Dinis Isakov <isakovdr@gmail.com>
2023-07-26 16:28:43 +0300

    create server 2

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,9 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  enableShutdownHooks(app: INestApplication) {
-    throw new Error('Method not implemented.');
-  }
   async onModuleInit() {
     await this.$connect();
   }
